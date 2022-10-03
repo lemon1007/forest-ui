c1113f0e651c5a40f813d6e3385fb2a088ab065d
青砚 <lemon19961007@sina.com>
2022-10-03 12:33:59 +0800

    一键部署github

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M master &&
+git branch -M main &&
 git remote add origin git@github.com:lemon19961007/forest-ui-website.git &&
-git push -f -u origin master &&
+git push -f -u origin main &&
 cd -
 echo https://lemon19961007.github.io/forest-ui-website/
